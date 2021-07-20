eec9e502155580a557f866ebcfafe0d507692ae6
surya04101998@gmail.com <surya04101998@gmail.com>
2021-07-20 09:41:05 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Fetcher from './Components/Fetcher';
 
 function App() {
   return (
     <div className="App">
       DinaKadhi
+      <Fetcher></Fetcher>
     </div>
   );
 }
---
+++ src/Components/Fetcher.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function Fetcher(props) {
+    const renderCont=" cont"
+    const today = fetch("https://api.prokerala.com/token?grant_type=client_credentials&client_id=a6ad1ed3-3558-45ac-b4fa-313bd6423165&client_secret=6IDzml9c8rbYIewgo3CqVbeS2eo7Hm7IqSDxWlxT")..then(res => res.json())
+        .then((data) => {
+            renderCont=data.access_token;
+        });
+
+    return <h1>Hello, {renderCont}</h1>;
+}
+
+export default Fetcher;
