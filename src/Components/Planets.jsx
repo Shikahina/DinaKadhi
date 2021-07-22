f9f522a7ca0fde242af65c9bee093b61f4a5a3a8
surya04101998@gmail.com <surya04101998@gmail.com>
2021-07-22 19:37:55 +0530

    transn

--- src/Components/Planets.jsx
+++ src/Components/Planets.jsx
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from 'react'
 import ReactJson from 'react-json-view';
 function Planets(props) {
-  const url = "http://planet-backend-application.herokuapp.com/getDiff";
+  const url = "https://planet-backend-application.herokuapp.com/getDiff";
   const [state, setstate] = useState();
   useEffect(() => {
     asyncawaitfunc()
